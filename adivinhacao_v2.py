b0224536c1c1e6569c4cdc94068e222393f6b784
João Gilberto <joaogilberto_ns@yahoo.com.br>
2021-11-26 17:04:06 -0300

    Iniciando o curso de Python - Alura

---
+++ adivinhacao_v2.py
@@ -0,0 +1,18 @@
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+
+chute = int(input("Digite seu número: "))
+# A função input sempre registra o valor informado como string a partir do Python 3, por isso,
+# é preciso especificar o tipo se for fazer algum tipo de compatação entre variáveis.
+
+print("Você digitou: ", chute)
+
+if chute == numero_secreto:
+    print("Você acertou!")
+else:
+    print("Você errou.")
+
+print("O número secreto é: ", numero_secreto)
